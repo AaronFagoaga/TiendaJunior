3d4b817c501e5ac2690d2e364d24ca8b82f6cb3f
AaronFagoaga <aaronf09@outlook.com>
2024-04-03 12:24:20 -0600

    Primer avance

---
+++ DEMO-TiendaJunior/DEMO-TiendaJunior/Models/PrecioModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DEMO_TiendaJunior.Models
+{
+    public class PrecioModel
+    {
+        public int Id_Precio { get; set; }
+
+        public int Id_Producto { get; set;}
+
+        public double PrecioUnidad { get; set;}
+
+        public ProductoModel? Producto { get; set; } //Captura datos del modelo producto para obtener nombre
+    }
+}
