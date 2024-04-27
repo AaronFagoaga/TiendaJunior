15c59977eed7c520f9d7e29f43e1b387fd91c6aa
AaronFagoaga <142861324+AaronFagoaga@users.noreply.github.com>
2024-04-27 11:31:22 -0600

    Respaldo

--- DEMO-TiendaJunior/DEMO-TiendaJunior/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DEMO_TiendaJunior.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
