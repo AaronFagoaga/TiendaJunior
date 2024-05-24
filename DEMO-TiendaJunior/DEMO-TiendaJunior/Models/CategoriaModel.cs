aa5ac966fbc56f77bb69f97e33a1375742c45c2c
zulma18 <zuzuguevara@gmail.com>
2024-05-24 14:26:15 -0600

    Validaciones Productos y Categorias

--- DEMO-TiendaJunior/DEMO-TiendaJunior/Models/CategoriaModel.cs
+++ DEMO-TiendaJunior/DEMO-TiendaJunior/Models/CategoriaModel.cs
@@ -1,9 +1,22 @@
-﻿namespace DEMO_TiendaJunior.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DEMO_TiendaJunior.Models
 {
     public class CategoriaModel
     {
         public int Id_Categoria { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre categoria es obligatorio.")]
+        [MinLength(5, ErrorMessage = "El nombre categoria debe tener al menos 5 letras.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre de la categoria solo puede contener letras y espacios.")]
+        [StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres.")]
         public string Nombre { get; set;}
+
+
+        [Required(ErrorMessage = "La descripcion es obligatorio.")]
+        [MinLength(15, ErrorMessage = "El nombre categoria debe tener al menos 15 letras.")]
+        [StringLength(75, ErrorMessage = "El nombre no puede tener más de 75 caracteres.")]
         public string Descripcion { get; set;}
 
     }
--- DEMO-TiendaJunior/DEMO-TiendaJunior/Models/ProductoModel.cs
+++ DEMO-TiendaJunior/DEMO-TiendaJunior/Models/ProductoModel.cs
@@ -6,14 +6,23 @@ namespace DEMO_TiendaJunior.Models
     {
         public int Id_Producto { get; set; }
 
+        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre del producto solo puede contener letras y espacios.")]
+        [MinLength(3, ErrorMessage = "El nombre producto debe tener al menos 3 letras.")]
+        [StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres.")]
         public string Nombre_Producto { get; set;}
 
+        [Required(ErrorMessage = "La presentacion es obligatorio.")]
+        [MinLength(2, ErrorMessage = "La presentacion debe tener al menos 2 letras.")]
+        [StringLength(30, ErrorMessage = "El nombre no puede tener 30 caracteres.")]
         public string Presentacion { get; set;}
 
-        [Required]
+        [Required(ErrorMessage = "El stock es obligatorio.")]
         [Range(0, int.MaxValue, ErrorMessage = "El campo Stock debe ser un número entero no negativo.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "El stock debe contener solo números.")]
         public string Stock { get; set;}
 
+        [Required(ErrorMessage = "Seleccionar la categoria es obligatorio.")]
         public int IdCategoria { get; set; }
         public CategoriaModel? Categoria { get; set;} //Captura los datos del modelo de categoria para obtener el nombre
     }
